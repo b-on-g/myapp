f70dff6153486c7f83bcbc2bce380db147453191
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-06-07 09:56:43 +0000

    Deploying to gh-pages from @ b-on-g/myapp@d84e4b58266e90a4d8a68f2d3589ad9e1eb5f442 🚀

--- web.js
+++ web.js
@@ -12488,7 +12488,7 @@ var $;
             }
             static default;
         }
-        return ((Class[Symbol.hasInstance] === $mol_schema_any[Symbol.hasInstance])
+        return ((Class?.[Symbol.hasInstance] === $mol_schema_any[Symbol.hasInstance])
             ? Class
             : $mol_schema_instance_);
     });
